aa263762d11a7b7fbf9460bd9983ff16f093c78f
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2017-12-21 18:41:41 +0300

    block added

---
+++ block.go
@@ -0,0 +1,29 @@
+package go_chain
+
+import (
+	"strconv"
+	"bytes"
+	"crypto/sha256"
+	"time"
+)
+
+type Block struct {
+	Timestamp     int64
+	Data          []byte
+	PrevBlockHash []byte
+	Hash          []byte
+}
+
+func NewBlock(data string, prevBlockHash []byte) *Block {
+	block := &Block{time.Now().Unix(), []byte(data), prevBlockHash, []byte{}}
+	block.SetHash()
+	return block
+}
+
+func (b *Block) SetHash() {
+	timestamp := []byte(strconv.FormatInt(b.Timestamp, 10))
+	headers := bytes.Join([][]byte{b.PrevBlockHash, b.Data, timestamp}, []byte{})
+	hash := sha256.Sum256(headers)
+
+	b.Hash = hash[:]
+}
