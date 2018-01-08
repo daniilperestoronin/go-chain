e5e72ff361a9d875edb70b82468d66db4fb1930a
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2018-01-08 18:28:28 +0300

    Add blockchain_iterator(cut from blockchain)

--- blockchain.go
+++ blockchain.go
@@ -284,36 +284,9 @@ Work:
 	return accumulated, unspentOutputs
 }
 
-// BlockchainIterator is used to iterate over blockchain blocks
-type BlockchainIterator struct {
-	currentHash []byte
-	db          *bolt.DB
-}
-
 // Iterator ...
 func (bc *Blockchain) Iterator() *BlockchainIterator {
 	bci := &BlockchainIterator{bc.tip, bc.db}
 
 	return bci
-}
-
-// Next returns next block starting from the tip
-func (i *BlockchainIterator) Next() *Block {
-	var block *Block
-
-	err := i.db.View(func(tx *bolt.Tx) error {
-		b := tx.Bucket([]byte(blocksBucket))
-		encodedBlock := b.Get(i.currentHash)
-		block = DeserializeBlock(encodedBlock)
-
-		return nil
-	})
-
-	if err != nil {
-		log.Panic(err)
-	}
-
-	i.currentHash = block.PrevBlockHash
-
-	return block
 }
