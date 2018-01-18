08e313a1fecdaad0c0238a179a486baa62fb988b
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2018-01-18 21:05:04 +0300

    Add crypto package in wallet

--- core/wallet.go
+++ core/wallet.go
@@ -8,6 +8,7 @@ import (
 	"crypto/sha256"
 	"log"
 
+	"github.com/daniilperestoronin/go-chain/crypto"
 	"golang.org/x/crypto/ripemd160"
 )
 
@@ -36,7 +37,7 @@ func (w Wallet) GetAddress() []byte {
 	checksum := checksum(versionedPayload)
 
 	fullPayload := append(versionedPayload, checksum...)
-	address := Base58Encode(fullPayload)
+	address := crypto.Base58Encode(fullPayload)
 
 	return address
 }
@@ -57,7 +58,7 @@ func HashPubKey(pubKey []byte) []byte {
 
 // ValidateAddress check if address if valid
 func ValidateAddress(address string) bool {
-	pubKeyHash := Base58Decode([]byte(address))
+	pubKeyHash := crypto.Base58Decode([]byte(address))
 	actualChecksum := pubKeyHash[len(pubKeyHash)-addressChecksumLen:]
 	version := pubKeyHash[0]
 	pubKeyHash = pubKeyHash[1 : len(pubKeyHash)-addressChecksumLen]
