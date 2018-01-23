cd14741315d36150f84063b2ac1dbff55652d95f
Daniil Perestoronin <perestoronin.daniil@gmail.com>
2018-01-23 17:26:56 +0300

    Change utils.ReverseBytes

--- utils/utils.go
+++ utils/utils.go
@@ -17,8 +17,9 @@ func IntToHex(num int64) []byte {
 }
 
 // ReverseBytes reverses a byte array
-func ReverseBytes(data []byte) {
+func ReverseBytes(data []byte) []byte {
 	for i, j := 0, len(data)-1; i < j; i, j = i+1, j-1 {
 		data[i], data[j] = data[j], data[i]
 	}
+	return data
 }
